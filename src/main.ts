65c76ca0ad4a8560f64d97785c1312f644093611
bao truong <truongquangbaolut0901@gmail.com>
2023-10-06 21:50:10 +0700

    final

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Thay đổi thành nguồn gốc của ứng dụng web của bạn
+    origin: `${process.env.CLIENT}`, // Thay đổi thành nguồn gốc của ứng dụng web của bạn
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Nếu bạn cần chia sẻ cookie hoặc thông tin xác thực qua CORS
   });
@@ -17,7 +17,7 @@ async function bootstrap() {
         enableImplicitConversion: true,
       }
     }),
-  );
+  )
   await app.listen(process.env.PORT || 8088);
 }
 bootstrap();
